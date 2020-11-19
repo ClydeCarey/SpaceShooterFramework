9eb27ea8593dfe90147287213f632c3c465d686b
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-18 17:30:45 -1000

    smoketrailconfusion
    really stressing on the smoke trails

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _missileSpeed = 8.0f;
+    [SerializeField]
+    private GameObject _smoke; 
+   
+    // Update is called once per frame
+    void Update()
+    {
+        MissileMoveUp();
+    }
+
+    void MissileMoveUp()
+    {
+        transform.Translate(Vector3.up * _missileSpeed * Time.deltaTime);
+
+        if (transform.position.y > 8.0f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+   
+}
